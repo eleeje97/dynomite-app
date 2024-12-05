919815c6190dd4d1d3e1c60795c8d5008b221c6d
danalee <eleeje97@gmail.com>
2024-12-05 11:11:22 +0900

    fix: 불필요한 import 제거

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button.tsx";
 import { Input } from "@/components/ui/input.tsx";
 import { Label } from "@/components/ui/label";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const navigate = useNavigate();
