8ea47669b9eaab60a35e5df2864cffaec85d6941
danalee <eleeje97@gmail.com>
2025-05-10 01:13:36 +0900

    feat: MainLayout 추가

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 # dynomite-app
+
 Dynomite 서비스 웹앱 프로젝트
+
+## Command
+
+```shell
+# 개발 실행
+yarn dev
+
+# 빌드
+yarn run build
+
+# 패키지 추가
+yarn add [패키지]
+
+# 패키지 다운로드
+yarn install
+
+# shadcn 컴포넌트 추기
+yarn dlx shadcn@latest add [컴포넌트]
+```
+
+## 참고
+
+https://ui.shadcn.com/docs/components
+
+---
 
 # React + TypeScript + Vite
 
@@ -7,8 +33,10 @@ This template provides a minimal setup to get React working in Vite with HMR and
 
 Currently, two official plugins are available:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md)
+  uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast
+  Refresh
 
 ## Expanding the ESLint configuration
 
@@ -18,17 +46,18 @@ If you are developing a production application, we recommend updating the config
 
 ```js
 export default tseslint.config({
-  languageOptions: {
-    // other options...
-    parserOptions: {
-      project: ['./tsconfig.node.json', './tsconfig.app.json'],
-      tsconfigRootDir: import.meta.dirname,
+    languageOptions: {
+        // other options...
+        parserOptions: {
+            project: ['./tsconfig.node.json', './tsconfig.app.json'],
+            tsconfigRootDir: import.meta.dirname,
+        },
     },
-  },
 })
 ```
 
-- Replace `tseslint.configs.recommended` to `tseslint.configs.recommendedTypeChecked` or `tseslint.configs.strictTypeChecked`
+- Replace `tseslint.configs.recommended` to `tseslint.configs.recommendedTypeChecked`
+  or `tseslint.configs.strictTypeChecked`
 - Optionally add `...tseslint.configs.stylisticTypeChecked`
 - Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and update the config:
 
@@ -37,17 +66,17 @@ export default tseslint.config({
 import react from 'eslint-plugin-react'
 
 export default tseslint.config({
-  // Set the react version
-  settings: { react: { version: '18.3' } },
-  plugins: {
-    // Add the react plugin
-    react,
-  },
-  rules: {
-    // other rules...
-    // Enable its recommended rules
-    ...react.configs.recommended.rules,
-    ...react.configs['jsx-runtime'].rules,
-  },
+    // Set the react version
+    settings: {react: {version: '18.3'}},
+    plugins: {
+        // Add the react plugin
+        react,
+    },
+    rules: {
+        // other rules...
+        // Enable its recommended rules
+        ...react.configs.recommended.rules,
+        ...react.configs['jsx-runtime'].rules,
+    },
 })
 ```
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -24,75 +24,71 @@ export default function Login() {
 
   return (
     <>
-      <div className="flex flex-col items-center content-center">
-        <div className="mt-24 text-center">
-          <h1 className="text-3xl font-semibold text-gray-800">Dynomite</h1>
-          <p className="text-gray-600 mt-2">
-            오늘도 멋지게 다이노해볼까요? 🦕🧨
-          </p>
-        </div>
+      <div className="text-center">
+        <h1 className="text-3xl font-semibold text-gray-800">Dynomite</h1>
+        <p className="text-gray-600 mt-2">오늘도 멋지게 다이노해볼까요? 🦕🧨</p>
+      </div>
 
-        {/*소셜로그인 버튼*/}
-        <div className="mt-10 mb-5 w-4/5 max-w-xl m-1">
-          <Button
-            variant="outline"
-            className="w-full max-w-xl m-1"
-          >
-            <FaGithub /> Login with GitHub
-          </Button>
-          <Button
-            variant="outline"
-            className="w-full max-w-xl m-1"
-          >
-            <FcGoogle /> Login with Google
-          </Button>
-          <Button
-            variant="outline"
-            className="w-full max-w-xl m-1"
-            onClick={onKakaoLoginBtnClick}
-          >
-            <RiKakaoTalkFill /> Login with Kakao
-          </Button>
-        </div>
+      {/*소셜로그인 버튼*/}
+      <div className="mt-10 mb-5 w-4/5 max-w-xl m-1">
+        <Button
+          variant="outline"
+          className="w-full max-w-xl m-1"
+        >
+          <FaGithub /> Login with GitHub
+        </Button>
+        <Button
+          variant="outline"
+          className="w-full max-w-xl m-1"
+        >
+          <FcGoogle /> Login with Google
+        </Button>
+        <Button
+          variant="outline"
+          className="w-full max-w-xl m-1"
+          onClick={onKakaoLoginBtnClick}
+        >
+          <RiKakaoTalkFill /> Login with Kakao
+        </Button>
+      </div>
 
-        {/*구분선*/}
-        <div className="flex items-center w-4/5 max-w-xl m-1">
-          <div className="flex-grow border-t border-border"></div>
-          <span className="px-2 text-sm text-muted-foreground">
-            Or Continue With
-          </span>
-          <div className="flex-grow border-t border-border"></div>
-        </div>
+      {/*구분선*/}
+      <div className="flex items-center w-4/5 max-w-xl m-1">
+        <div className="flex-grow border-t border-border"></div>
+        <span className="px-2 text-sm text-muted-foreground">
+          Or Continue With
+        </span>
+        <div className="flex-grow border-t border-border"></div>
+      </div>
 
-        {/*자체로그인*/}
-        <div className="mt-3 w-4/5 max-w-xl m-1">
-          <Input
-            type="text"
-            placeholder="username"
-            className="w-full max-w-xl m-1"
-          ></Input>
-          <Input
-            type="password"
-            placeholder="password"
-            className="w-full max-w-xl m-1"
-          ></Input>
+      {/*자체로그인*/}
+      <div className="mt-3 w-4/5 max-w-xl m-1">
+        <Input
+          type="text"
+          placeholder="username"
+          className="w-full max-w-xl m-1"
+        ></Input>
+        <Input
+          type="password"
+          placeholder="password"
+          className="w-full max-w-xl m-1"
+        ></Input>
+        <Button
+          className="w-full max-w-xl m-1 shadow-lg"
+          onClick={onSignInBtnClick}
+        >
+          Login
+        </Button>
+        {/*회원가입*/}
+        <div className="flex-row text-center">
+          <Label>아직 회원이 아니신가요?</Label>
           <Button
-            className="w-full max-w-xl m-1 shadow-lg"
-            onClick={onSignInBtnClick}
+            className="font-semibold"
+            variant="link"
+            onClick={onSignUpBtnClick}
           >
-            Login
+            회원가입하기
           </Button>
-          {/*회원가입*/}
-          <div className="flex-row text-center">
-            <Label>아직 회원이 아니신가요?</Label>
-            <Button
-              className="font-semibold"
-              variant="link"
-              onClick={onSignUpBtnClick}
-            >
-              회원가입하기
-            </Button>
-          </div>
         </div>
       </div>
     </>
